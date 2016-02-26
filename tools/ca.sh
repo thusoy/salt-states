1769f89ab34fa3e05d4af247705225b6a65c0781
Tarjei Husøy <git@thusoy.com>
2016-02-25 16:46:32 -0800

    Sorta working root CA generation
    Currently writes plaintext private key to disk, must be fixed.

--- tools/ca.sh
+++ tools/ca.sh
@@ -3,37 +3,60 @@
 
 set -e
 
-PKI_DIR=/tmp
+PKI_DIR=/tmp/ca
 
 function create-directory-structure () {
     mkdir -p $PKI_DIR/ca/private
     mkdir -p $PKI_DIR/certs
 }
 
+
 function new-root () {
     create-directory-structure
+    dump-openssl.cnf
 
-    root_name=$1
+    local root_name="$1"
+    mkdir -p "$PKI_DIR/ca/$root_name/"{private,db}
+    touch "$PKI_DIR/ca/$root_name/db/db"
+    echo 01 > "$PKI_DIR/ca/$root_name/db/serial"
+    echo 01 > "$PKI_DIR/ca/$root_name/db/crl.srl"
     echo -n "Enter new root CA common name: "
     read ca_common_name
-    openssl ecparam -name secp256k1 -genkey \
-    | openssl pkcs8 -v2 aes-256-cbc \
-              -topk8 \
-              -out $PKI_DIR/ca/$root_name/private/$root_name.key
+    local private_key="$PKI_DIR/ca/$root_name/private/$root_name.key"
+    openssl ecparam -name secp256k1 -genkey -out "$private_key"
     export CA_NAME="$ca_common_name"
     export SHORT_NAME="$root_name"
-    openssl req -new \
-                -config $PKI_DIR/openssl.cnf \
-                -out /tmp/$root_name.csr \
-                -key $PKI_DIR/ca/$root_name/private/$root_name.key
+    cat <<EOF
+[INFO] Now we'll create the CSR, enter the key you just created.
+EOF
+    local old_dir=$(pwd)
+    cd "$PKI_DIR"
+    local csr=$(openssl req -new \
+                -config "$PKI_DIR/openssl.cnf" \
+                -key "$private_key" \
+                -nodes)
+    cat <<EOF
+[INFO] We'll now self-sign our root certificate, enter the key you just created again.
+EOF
     MODE=root openssl ca -selfsign \
-                              -config $PKI_DIR/openssl.cnf \
-                              -in /tmp/$root_name.csr \
-                              -out $PKI_DIR/certs/$root_name.crt \
-                              -extensions root_ca_ext \
-                              -days 7000 \
-                              -notext
-    rm /tmp/$root_name.csr
+                         -config "$PKI_DIR/openssl.cnf" \
+                         -in <(echo "$csr") \
+                         -out "$PKI_DIR/certs/$root_name.crt" \
+                         -extensions root_ca_ext \
+                         -days 7300 \
+                         -notext
+
+    # store the private to disk encrypted
+    cat <<EOF
+[INFO] You'll now be asked for the passphrase under which you want to encrypt your new
+       root CA. Generate a strong one and store it securely.
+
+       Hint: You can create strong keys by using "openssl rand -base64 30"
+EOF
+    cat "$private_key" | openssl pkcs8 -v2 aes-256-cbc \
+                  -topk8 \
+                  -out "$PKI_DIR/ca/$root_name/private/$root_name.key"
+    cd "$old_dir"
 }
 
 function create-user-ca () {
@@ -214,9 +237,159 @@ function show-help () {
     echo "   revoke-user <username>"
 }
 
+function dump-openssl.cnf () {
+    cat <<"EOF" > $PKI_DIR/openssl.cnf
+# Simple Root CA
+
+# The [default] section contains global constants that can be referred to from
+# the entire configuration file. It may also hold settings pertaining to more
+# than one openssl command.
+
+MODE                    = root # Set root to act as root CA
+mode                    = ${ENV::MODE}
+CA_NAME                 = default-ca-name
+ca_name                 = ${ENV::CA_NAME}
+ca                      = ${ENV::SHORT_NAME}              # CA name
+dir                     = .                   # Top dir
+
+# The next part of the configuration file is used by the openssl req command.
+# It defines the CA's key pair, its DN, and the desired extensions for the CA
+# certificate.
+
+[ req ]
+encrypt_key             = yes                   # Protect private key
+default_md              = sha256                # MD to use
+utf8                    = yes                   # Input is UTF-8
+string_mask             = utf8only              # Emit UTF-8 strings
+prompt                  = no                    # Don't prompt for DN
+distinguished_name      = ca_dn                 # DN section
+req_extensions          = ca_reqext             # Desired extensions
+
+[ ca_dn ]
+organizationName        = "Megacool"
+organizationalUnitName  = "Megacool Internal Infrastructure"
+commonName              = $ca_name
+
+[ ca_reqext ]
+keyUsage                = critical,keyCertSign,cRLSign
+basicConstraints        = critical,CA:true
+subjectKeyIdentifier    = hash
+
+# The remainder of the configuration file is used by the openssl ca command.
+# The CA section defines the locations of CA assets, as well as the policies
+# applying to the CA.
+
+[ ca ]
+default_ca              = ${mode}_ca               # The default CA section
+
+[ root_ca ]
+certificate             = $dir/certs/$ca.crt       # The CA cert
+private_key             = $dir/ca/$ca/private/$ca.key # CA private key
+new_certs_dir           = $dir/ca/$ca           # Certificate archive
+serial                  = $dir/ca/$ca/db/serial # Serial number file
+crlnumber               = $dir/ca/$ca/db/crl.srl # CRL number file
+database                = $dir/ca/$ca/db/db     # Index file
+unique_subject          = no                    # Require unique subject
+default_days            = 365                   # How long to certify for
+default_md              = sha512                # MD to use
+policy                  = match_pol             # Default naming policy
+email_in_dn             = no                    # Add email to cert DN
+preserve                = no                    # Keep passed DN ordering
+name_opt                = ca_default            # Subject DN display options
+cert_opt                = ca_default            # Certificate display options
+copy_extensions         = none                  # Copy extensions from CSR
+x509_extensions         = signing_ca_ext        # Default cert extensions
+default_crl_days        = 365                   # How long before next CRL
+crl_extensions          = crl_ext               # CRL extensions
+
+# Naming policies control which parts of a DN end up in the certificate and
+# under what circumstances certification should be denied.
+
+[ match_pol ]
+organizationName        = match                 # Must match 'Simple Inc'
+organizationalUnitName  = optional              # Included if present
+commonName              = supplied              # Must be present
+
+[ any_pol ]
+domainComponent         = optional
+countryName             = optional
+stateOrProvinceName     = optional
+localityName            = optional
+organizationName        = optional
+organizationalUnitName  = optional
+commonName              = optional
+emailAddress            = optional
+
+# Certificate extensions define what types of certificates the CA is able to
+# create.
+
+[ root_ca_ext ]
+keyUsage                = critical,keyCertSign,cRLSign
+basicConstraints        = critical,CA:true
+subjectKeyIdentifier    = hash
+authorityKeyIdentifier  = keyid:always
+crlDistributionPoints   = user_crl, root_crl
+
+[ signing_ca_ext ]
+keyUsage                = critical,keyCertSign,cRLSign
+basicConstraints        = critical,CA:true,pathlen:0
+subjectKeyIdentifier    = hash
+authorityKeyIdentifier  = keyid:always
+crlDistributionPoints   = user_crl, root_crl
+
+[ server_ext ]
+keyUsage                = critical,digitalSignature,keyEncipherment
+basicConstraints        = CA:false
+extendedKeyUsage        = serverAuth
+subjectKeyIdentifier    = hash
+authorityKeyIdentifier  = keyid:always
+crlDistributionPoints   = user_crl, root_crl
+
+[ user_crl ]
+fullname = URI:https://vpn.thusoy.com/user-crl.pem
+reasons  = privilegeWithdrawn, keyCompromise
+
+[ root_crl ]
+fullname  = URI:https://vpn.thusoy.com/root-crl.pem
+reasons   = CACompromise, superseded, cessationOfOperation
+CRLIssuer = dirName:root_crl_issuer
+
+[ root_crl_issuer ]
+O = thusoy.com
+OU = thusoy.com VPN
+CN = vpn.thusoy.com
+
+[ signing_ca ]
+certificate             = $dir/ca/$ca.crt       # The CA cert
+private_key             = $dir/ca/$ca/private/$ca.key # CA private key
+new_certs_dir           = $dir/certs            # Certificate archive
+serial                  = $dir/ca/$ca/db/serial # Serial number file
+crlnumber               = $dir/ca/$ca/db/crl.srl # CRL number file
+database                = $dir/ca/$ca/db/db     # Index file
+unique_subject          = no                    # Require unique subject
+default_days            = 365                   # How long to certify for
+default_md              = sha256                # MD to use
+policy                  = match_pol             # Default naming policy
+email_in_dn             = no                    # Add email to cert DN
+preserve                = no                    # Keep passed DN ordering
+name_opt                = ca_default            # Subject DN display options
+cert_opt                = ca_default            # Certificate display options
+copy_extensions         = copy                  # Copy extensions from CSR
+default_crl_days        = 365                   # How long before next CRL
+crl_extensions          = crl_ext               # CRL extensions
+
+# CRL extensions exist solely to point to the CA certificate that has issued
+# the CRL.
+
+[ crl_ext ]
+authorityKeyIdentifier  = keyid:always,issuer:always
+EOF
+}
+
+
 case $1 in
     new-root)
-        new-root $2
+        new-root "$2"
     ;;
     create-user-ca)
         create-user-ca $2
