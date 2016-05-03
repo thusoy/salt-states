086f68d8631fb988ad01c09bc4a33e0fc6b34dc8
Tarjei Husøy <git@thusoy.com>
2016-05-02 21:17:42 -0700

    Add mutt state

---
+++ salt/mutt/source-local-dotmutt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+# Source all the config files in ~/.mutt
+config_dir=~/.mutt
+test -d "$config_dir" && ~/find "$config_dir" \
+    -type f \
+    -name "*.rc" \
+    -readable \
+    -exec echo source \"{}\" \;
