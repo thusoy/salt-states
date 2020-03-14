15c2e2ba19b4551108ab47a4d7809a618b8d64ce
Tarjei Husøy <git@thusoy.com>
2020-03-14 15:07:24 -0700

    Filter private ips in external_ips

--- extensions/pillar/external_ips.py
+++ extensions/pillar/external_ips.py
@@ -4,9 +4,16 @@ the minion, like GCE and AWS. This is similar to minion_ips, but exposes the IP
 to the minion owning it.
 '''
 
-def ext_pillar(minion_id, pillar, minion_ip_path='/etc/salt/minion_ips', **kwargs):
+try:
+    import ipaddress
+    HAS_IPADDRESS = True
+except ImportError:
+    HAS_IPADDRESS = False
+
+
+def ext_pillar(minion_id, pillar, minion_ips_path='/etc/salt/minion_ips', **kwargs):
     try:
-        fh = open(minion_ip_path)
+        fh = open(minion_ips_path)
     except:
         return {}
 
@@ -14,8 +21,17 @@ def ext_pillar(minion_id, pillar, minion_ip_path='/etc/salt/minion_ips', **kwarg
         for line in fh:
             found_minion_id, minion_ip = line.strip().split(None, 1)
             if found_minion_id == minion_id:
+                all_ips = minion_ip.split()
+                external_ips = []
+                if HAS_IPADDRESS:
+                    for ip in all_ips:
+                        parsed_ip = ipaddress.ip_address(ip)
+                        if parsed_ip.is_global:
+                            external_ips.append(ip)
+                else:
+                    external_ips.extend(all_ips)
                 return {
-                    'external_ips': minion_ip.split(),
+                    'external_ips': external_ips,
                 }
 
     return {}
