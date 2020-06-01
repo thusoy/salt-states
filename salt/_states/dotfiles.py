ca75b9516591deed779be02c34feb3d559689314
Tarjei Husøy <git@thusoy.com>
2020-06-01 00:08:31 -0700

    Refactor to reuse pull

--- salt/_states/dotfiles.py
+++ salt/_states/dotfiles.py
@@ -27,28 +27,21 @@ def repo(name, repo, user):
     branch = branch or 'master'
     home_dir = os.path.expanduser('~%s' % user)
     git_dir = os.path.expanduser('~%s/%s' % (user, DOTFILES_GIT_DIR))
-    if os.path.exists(git_dir):
-        try:
-            changes = _pull_repo(home_dir, git_dir, branch)
-        except subprocess.CalledProcessError as error:
-            ret['result'] = False
-            ret['comment'] = error.stderr.decode('utf-8')
-            return ret
+    try:
+        if not os.path.exists(git_dir):
+            _clone_new_repo(home_dir, git_dir, repo_url, branch)
+            ret['comment'] = 'Cloned branch %s from repo %s\n' % (branch, repo_url)
 
-        if changes:
-            ret['comment'] = 'The following dotfiles were updated'
-            ret['changes']['changes'] = changes
-    else:
-        try:
-            diff = _clone_new_repo(home_dir, git_dir, repo_url, branch)
-        except subprocess.CalledProcessError as error:
-            ret['result'] = False
-            if error.stdout:
-                ret['comment'] += 'stdout: %s\n' % error.stdout.decode('utf-8')
-            if error.stderr:
-                ret['comment'] += 'stderr: %s' % error.stderr.decode('utf-8')
-            return ret
-        ret['changes']['changes'] = diff
+        changes = _pull_repo(home_dir, git_dir, branch)
+    except subprocess.CalledProcessError as error:
+        ret['result'] = False
+        ret['comment'] += 'stderr: %s\n' % error.stderr.decode('utf-8')
+        return ret
+
+    if changes:
+        ret['comment'] += 'The following dotfiles were updated'
+        ret['changes']['changes'] = changes
+
     return ret
 
 
@@ -84,24 +77,12 @@ def _clone_new_repo(home_dir, git_dir, repo, branch):
             'git',
             'clone', repo,
             tempdir,
-            # '--quiet',
-        ], stderr=subprocess.PIPE)
-        subprocess.check_call([
-            'git',
-            '--git-dir', git_dir,
-            'fetch',
-            '--all',
             '--quiet',
-        ], cwd=home_dir, stderr=subprocess.PIPE)
+        ])
         os.rename(os.path.join(tempdir, '.git'), git_dir)
-        changes = subprocess.check_output([
-            'git',
-            '--git-dir', git_dir,
-            'diff', '-R',
-        ], stderr=subprocess.PIPE)
         subprocess.check_call([
             'git',
             '--git-dir', git_dir,
-            'checkout', '.'
-        ], cwd=home_dir, stderr=subprocess.PIPE)
-        return changes
+            'config',
+            'status.showUntrackedFiles', 'no',
+        ], cwd=home_dir)
