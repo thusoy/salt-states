601a94a16192d2a730e7c31b3877a3d15cf294ad
Tarjei Husøy <git@thusoy.com>
2020-05-31 22:08:41 -0700

    Parse branch from url

--- salt/_states/dotfiles.py
+++ salt/_states/dotfiles.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 import subprocess
 import shutil
+import urllib.parse
 
 import salt.utils.path
 
@@ -10,7 +11,9 @@ DOTFILES_GIT_DIR = '.dotfiles'
 
 
 def __virtual__():
-    return salt.utils.path.which('git') is not None
+    if salt.utils.path.which('git'):
+        return True
+    return False, 'Missing a git binary'
 
 
 def repo(name, repo, user):
@@ -20,53 +23,85 @@ def repo(name, repo, user):
         'result': True,
         'changes': {},
     }
+    repo_url, branch = urllib.parse.urldefrag(repo)
+    branch = branch or 'master'
     home_dir = os.path.expanduser('~%s' % user)
     git_dir = os.path.expanduser('~%s/%s' % (user, DOTFILES_GIT_DIR))
     if os.path.exists(git_dir):
-        changes, comment = _pull_repo(home_dir, git_dir)
+        try:
+            changes = _pull_repo(home_dir, git_dir, branch)
+        except subprocess.CalledProcessError as error:
+            ret['result'] = False
+            ret['comment'] = error.stderr.decode('utf-8')
+            return ret
+
         if changes:
+            ret['comment'] = 'The following dotfiles were updated'
             ret['changes']['changes'] = changes
-        ret['comment'] = comment
     else:
-        _clone_new_repo(home_dir, git_dir, repo)
-        ret['changes']['new'] = 'Cloned repo %s' % repo
+        try:
+            diff = _clone_new_repo(home_dir, git_dir, repo_url, branch)
+        except subprocess.CalledProcessError as error:
+            ret['result'] = False
+            if error.stdout:
+                ret['comment'] += 'stdout: %s\n' % error.stdout.decode('utf-8')
+            if error.stderr:
+                ret['comment'] += 'stderr: %s' % error.stderr.decode('utf-8')
+            return ret
+        ret['changes']['changes'] = diff
     return ret
 
 
-def _pull_repo(home_dir, git_dir):
-    old_head = subprocess.check_output([
+def _pull_repo(home_dir, git_dir, branch):
+    subprocess.check_call([
         'git',
         '--git-dir', git_dir,
-        'rev-parse', 'HEAD'
+        'fetch',
+        '--all',
+        '--quiet',
     ], cwd=home_dir)
-    proc = subprocess.run([
+    changes = subprocess.check_output([
         'git',
         '--git-dir', git_dir,
-        'pull',
-    ], cwd=home_dir, check=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    new_head = subprocess.check_output([
+        'diff',
+        '-R',
+        'origin/%s' % branch,
+    ], cwd=home_dir)
+    subprocess.check_call([
         'git',
         '--git-dir', git_dir,
-        'rev-parse', 'HEAD'
+        'reset',
+        '--hard',
+        'origin/%s' % branch,
     ], cwd=home_dir)
 
-    changes = ''
-    if new_head != old_head:
-         changes = proc.stdout.decode('utf-8')
-    comment = proc.stderr.decode('utf-8')
-    return changes, comment
+    return changes
 
 
-def _clone_new_repo(home_dir, git_dir, repo):
+def _clone_new_repo(home_dir, git_dir, repo, branch):
     with tempfile.TemporaryDirectory(dir=home_dir) as tempdir:
         subprocess.check_call([
             'git',
             'clone', repo,
             tempdir,
-        ])
+            # '--quiet',
+        ], stderr=subprocess.PIPE)
+        subprocess.check_call([
+            'git',
+            '--git-dir', git_dir,
+            'fetch',
+            '--all',
+            '--quiet',
+        ], cwd=home_dir, stderr=subprocess.PIPE)
         os.rename(os.path.join(tempdir, '.git'), git_dir)
+        changes = subprocess.check_output([
+            'git',
+            '--git-dir', git_dir,
+            'diff', '-R',
+        ], stderr=subprocess.PIPE)
         subprocess.check_call([
             'git',
             '--git-dir', git_dir,
             'checkout', '.'
-        ], cwd=home_dir)
+        ], cwd=home_dir, stderr=subprocess.PIPE)
+        return changes
